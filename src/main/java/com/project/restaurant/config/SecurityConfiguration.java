037ff66f40afee1d7fb02749ad26b3db54e4ed01
Juan Ignacio Caprioli <juanicaprioli16@outlook.com>
2024-08-10 17:27:53 -0300

    Added menu

--- src/main/java/com/project/restaurant/config/SecurityConfiguration.java
+++ src/main/java/com/project/restaurant/config/SecurityConfiguration.java
@@ -1,73 +1,3 @@
-//package com.project.restaurant.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-//import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-//import org.springframework.security.oauth2.jwt.JwtDecoder;
-//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-//import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@Configuration
-//@EnableMethodSecurity
-//public class SecurityConfiguration {
-//
-//    @Value("${okta.oauth2.issuer}")
-//    private String issuerUri;
-//
-//    @Bean
-//    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-//        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-//        requestHandler.setCsrfRequestAttributeName(null);
-//        http.authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers(HttpMethod.GET, "api/tenant-listing/get-all-by-category").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "api/tenant-listing/get-one").permitAll()
-//                        .requestMatchers(HttpMethod.POST, "api/tenant-listing/search").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "api/booking/check-availability").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "assets/*").permitAll()
-//                        .anyRequest()
-//                        .authenticated())
-//                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//                        .csrfTokenRequestHandler(requestHandler))
-//                .oauth2Login(Customizer.withDefaults())
-//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-//                .oauth2Client(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public JwtDecoder jwtDecoder() {
-//        return NimbusJwtDecoder.withJwkSetUri(issuerUri).build();
-//    }
-//
-//    @Bean
-//    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
-//        return authorities -> {
-//            Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-//
-//            authorities.forEach(grantedAuthority -> {
-//                if (grantedAuthority instanceof OidcUserAuthority oidcUserAuthority) {
-//                    grantedAuthorities
-//                            .addAll(SecurityUtils.extractAuthorityFromClaims(oidcUserAuthority.getUserInfo().getClaims()));
-//                }
-//            });
-//            return grantedAuthorities;
-//        };
-//    }
-//}
-
 package com.project.restaurant.config;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -77,8 +7,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
@@ -99,21 +27,15 @@ public class SecurityConfiguration {
     private String issuerUri;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName(null);
-
-        http
-                .authorizeHttpRequests(authorize -> authorize
+        http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/api/menus/**").permitAll() // Permite el acceso a los endpoints de menú
-                        .requestMatchers(HttpMethod.GET, "/api/tenant-listing/get-all-by-category").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/tenant-listing/get-one").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/tenant-listing/search").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/booking/check-availability").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/assets/**").permitAll()
-                        .anyRequest().authenticated())
-                .csrf(csrf -> csrf
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .requestMatchers(HttpMethod.GET, "assets/*").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                         .csrfTokenRequestHandler(requestHandler))
                 .oauth2Login(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
@@ -141,12 +63,4 @@ public class SecurityConfiguration {
             return grantedAuthorities;
         };
     }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/assets/**");
-    }
 }
-
-
-
